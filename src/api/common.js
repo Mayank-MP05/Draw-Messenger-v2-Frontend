521486a9b71cff56c6cf5da325a6dcddf3279e22
Mayank-MP05 <mayank5pande@gmail.com>
2022-07-26 21:52:08 +0530

    #3 Axios Common client added for smoother request

--- src/api/common.js
+++ src/api/common.js
@@ -1,1 +1,11 @@
+import axios from "axios";
 
+const commonAPIClient = axios.create({
+  baseURL: "https://localhost:9000/",
+  timeout: 3000,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+export default commonAPIClient;
