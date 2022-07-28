01322c94e27800aa5ebd16563fcb0ca0f7403d19
Mayank-MP05 <mayank5pande@gmail.com>
2022-07-28 20:11:46 +0530

    Group Card added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import GroupCard from "./components/homepage/group-card";
+import UserCard from "./components/homepage/user-card";
 import Navbar from "./components/navbar";
 import "./index.scss";
 import Routes from "./routes";
@@ -6,8 +8,15 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-row bg-black rounded-lg mt-4 mx-auto w-[80%]">
-        lorem ipsum dolor sit amet, consectetur adip
+      <div className="flex flex-row rounded-xl mt-6 mx-auto w-[80%] shadow-2xl">
+        <div className="w-4/12 p-2 hidden md:block">
+          {" "}
+          <UserCard />
+        </div>
+        <div className="md:w-8/12 p-2 sm:w-full">
+          <h2 className="text-lg font-bold m-2">Groups</h2>
+          <GroupCard />
+        </div>
       </div>
     </>
   );
