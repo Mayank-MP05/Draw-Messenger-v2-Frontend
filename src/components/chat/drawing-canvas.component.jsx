bcb21581fe227160ca2b58e47064068c462445fd
Mayank-MP05 <mayank5pande@gmail.com>
2022-08-03 23:46:48 +0530

    #9 Drawing Canvas working as Expected

--- src/components/chat/drawing-canvas.component.jsx
+++ src/components/chat/drawing-canvas.component.jsx
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 import { SketchPicker, CirclePicker } from "react-color";
 import ColorPickerComponent from "./color-picker.component";
 import CanvasDraw from "react-canvas-draw";
-
+// import InputRange from "react-input-range";
+import { RangeStepInput } from "react-range-step-input";
 const DrawingCanvasComponent = () => {
-  const [colorSelected, setcolorSelected] = useState("#3f51b5");
+  const [colorSelected, setColorSelected] = useState("#3f51b5");
+  const [brushRadius, setBrushRadius] = useState(5);
   const [saveableCanvas, setSaveableCanvas] = useState(null);
+
   const handleColorChange = (color) => {
-    setcolorSelected(color.hex);
+    setColorSelected(color.hex);
   };
   return (
     <div className="h-40 w-full flex flex-row bg-white rounded-lg border-2 shadow-lg p-2">
       <div className="flex flex-col">
-        <h5>Choose the Color:</h5>
+        <h5 className="text-bold font-lg p-1">Choose the Color:</h5>
         <CirclePicker
           width={`150px`}
           colors={[
@@ -33,19 +36,27 @@ const DrawingCanvasComponent = () => {
       <div className="flex flex-col">
         <CanvasDraw
           ref={(canvasDraw) => setSaveableCanvas(canvasDraw)}
-          onChange={(e) => console.log(e)}
+          //   onChange={(e) => console.log(e)}
           brushColor={colorSelected}
+          brushRadius={brushRadius}
           canvasWidth={400}
           canvasHeight={400}
         />
       </div>
-      <div className="flex flex-col btn-group justify-center items-center mx-auto">
+      <div className="flex flex-col btn-group justify-ce nter items-center mx-auto">
+        <h5 className="text-bold font-lg p-1">Brush Size</h5>
+        <RangeStepInput
+          max={"25"}
+          min={"2"}
+          value={`${brushRadius}`}
+          onChange={(e) => setBrushRadius(e.target.value)}
+        />
         <button
           onClick={() => {
-            localStorage.setItem("savedDrawing", saveableCanvas.getSaveData());
+            console.log("Send Img");
           }}
         >
-          Save
+          Send
         </button>
         <button
           onClick={() => {
