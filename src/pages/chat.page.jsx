028f28be40bfb4a8a09171895c4919c4f314d5c8
Mayank-MP05 <mayank5pande@gmail.com>
2022-08-14 11:39:30 +0530

    #10 Socket IO added on Client Side

--- src/pages/chat.page.jsx
+++ src/pages/chat.page.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
 import ChatComponent from "../components/chat/chat.component";
 import UserCard from "../components/homepage/user-card";
 import Navbar from "../components/common/navbar";
@@ -8,10 +10,20 @@ const ChatViewHomePage = ({ userHandler, groupHandler }) => {
   const [loggedInUser, setLoggedInUser] = userHandler;
 
   const { id } = useParams();
-  console.log("group id: ", id);
+
+  useEffect(() => {
+    const socket = io("http://localhost:9001");
+    console.log(socket);
+    socket.emit("chat", { world: "game of thrones" }, (response) => {
+      console.log(response); // "got it"
+    });
+    socket.on("chat", (payload) => {
+      console.log(payload);
+    });
+  }, []);
   return (
     <>
-      <div className="flex flex-row rounded-xl mt-6 mx-auto w-[95%] md:w-[80%] shadow-2xl">
+      <div className="flex flex-row rounded-xl md:mt-6 mx-auto w-[95%] md:w-[80%] shadow-2xl">
         <div className="w-4/12 p-2 hidden md:block">
           {" "}
           <UserCard userHandler={userHandler} />
