29237b3ae66130d7fd637292977924d3d26c970d
Mayank-MP05 <mayank5pande@gmail.com>
2022-08-16 23:18:50 +0530

    Removed unncessary message tiles #16

--- src/components/chat/chat.component.jsx
+++ src/components/chat/chat.component.jsx
@@ -39,43 +39,6 @@ const ChatComponent = ({ groupHandler, userHandler, messageList }) => {
               setMessagesEnd(el);
             }}
           ></div>
-          <MessageTile
-            isLoading={false}
-            msgData={{
-              type: "TEXT",
-              createdAt: new Date().getTime(),
-              createdBy: "wghfbsjfddfsf",
-              groupId: "sdfkjsfhksd",
-              content: "Message one from Chat.component.jsx",
-            }}
-            timestamp={"01:30PM"}
-          />
-          <MessageTile
-            isLoading={false}
-            msgData={{
-              type: "LINK",
-              createdAt: new Date().getTime(),
-              createdBy: "wghfbsjfddfsf",
-              groupId: "sdfkjsfhksd",
-              content: "Message two from Chat.component.jsx 😂😂",
-              title: "Google.ocm",
-            }}
-            timestamp={"01:30PM"}
-          />
-          <MessageTile
-            isLoading={false}
-            msgData={{
-              type: "DRAWING",
-              createdAt: new Date().getTime(),
-              createdBy: "wghfbsjfddfsf",
-              groupId: "sdfkjsfhksd",
-              content: "Message two from Chat.component.jsx 😂😂",
-              title: "Google.ocm",
-              imgUrl:
-                "https://images.unsplash.com/photo-1627483262268-9c2b5b2834b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8OHx8YmFja2dyb3VuZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-            }}
-            timestamp={"01:30PM"}
-          />
         </ul>
       </div>
       <MessageWritePanel
